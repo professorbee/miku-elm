d6599b8a17e6569f9f17b05dff443f032d3fa1d1
professorbee <batquartet@gmail.com>
2021-03-12 14:06:17 -0500

    init

---
+++ src/Main.elm
@@ -0,0 +1,80 @@
+module Main exposing (..)
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode exposing (Decoder, field, string)
+main : Program () Model Msg
+main =
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
+
+type Model
+  = Failure
+  | Loading
+  | Success String
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  (Loading, getRandomCatGif)
+
+type Msg
+  = MorePlease
+  | GotGif (Result Http.Error String)
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    MorePlease ->
+      (Loading, getRandomCatGif)
+
+    GotGif result ->
+      case result of
+        Ok url ->
+          (Success url, Cmd.none)
+
+        Err _ ->
+          (Failure, Cmd.none)
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ viewGif model ]
+
+viewGif : Model -> Html Msg
+viewGif model =
+  case model of
+    Failure ->
+      div []
+        [ text "I could not load a random cat for some reason. "
+        , button [ onClick MorePlease ] [ text "Try Again!" ]
+        ]
+
+    Loading ->
+      text "Loading..."
+
+    Success url ->
+      div []
+        [ img [ src url ] [],
+        div [ class "text-center"] [button [ onClick MorePlease] [ text "More Please!" ]]
+        ]
+
+getRandomCatGif : Cmd Msg
+getRandomCatGif =
+  Http.get
+    { url = "https://plushmiku.xyz/api/random"
+    , expect = Http.expectJson GotGif gifDecoder
+    }
+
+gifDecoder : Decoder String
+gifDecoder =
+  field "username" string
